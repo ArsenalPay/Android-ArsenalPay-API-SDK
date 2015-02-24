2161c88ab61b14a5d9656f426b04cf9feb6c8c82
Yaroslav Shchegolevatykh <dejibqp@mail.ru>
2015-02-24 17:37:56 +1100

    fixed HttpUrlConnectionImpl

--- src/main/java/ru/arsenalpay/api/client/impl/HttpUrlConnectionImpl.java
+++ src/main/java/ru/arsenalpay/api/client/impl/HttpUrlConnectionImpl.java
@@ -11,6 +11,8 @@ import ru.arsenalpay.api.util.RequestUtils;
 
 import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,7 +21,7 @@ import java.util.logging.Logger;
 public class HttpUrlConnectionImpl implements ApiClient {
 
     private final static Logger log = Logger.getLogger(HttpUrlConnectionImpl.class.getName());
-    private HttpUrlConfiguration configuration;
+    private final HttpUrlConfiguration configuration;
 
     public HttpUrlConnectionImpl() {
         configuration = HttpUrlConfiguration.getDefaultConfiguration();
@@ -51,94 +53,37 @@ public class HttpUrlConnectionImpl implements ApiClient {
 
     /**
      * Simply execute HTTP GET request
+     *
      * @param command -- api command
      * @return apiResponse -- apiResponse
      */
-    private ApiResponse executeGet(ApiCommand command) {
+    private ApiResponse executeGet(ApiCommand command) throws IOException {
 
-        HttpURLConnection connection = null;
-        URL url = null;
+        HttpURLConnection connection = getConnection(command);
         try {
-            url = new URL(command.getFullUri());
-        } catch (Exception e) {
-            log.log(Level.SEVERE, "ArsenalpayAPI-SDK HttpUrlConnectionImpl:doGet", e);
-        }
-
-        try {
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            if (configuration != null) {
-                connection.setReadTimeout(configuration.getReadTimeout());
-                connection.setConnectTimeout(configuration.getConnectionTimeout());
-            }
             return getResponse(connection);
-        } catch (Exception e) {
-            log.log(Level.SEVERE, "ArsenalpayAPI-SDK HttpUrlConnectionImpl:doGet", e);
         } finally {
-            connection.disconnect();
+            if (connection != null) {
+                connection.disconnect();
+            }
         }
-        return ApiResponseImpl.createEmpty();
     }
+
     /**
      * Simply execute HTTP POST request
+     *
      * @param command -- api command
      * @return apiResponse -- apiResponse
      */
-    private ApiResponse executePost(ApiCommand command) {
+    private ApiResponse executePost(ApiCommand command) throws IOException {
 
-        HttpURLConnection connection = null;
-        URL url = null;
-        OutputStream output;
-        PrintWriter printWriter = null;
-        String size = null;
-        String str = null;
-
-        try {
-            url = new URL(command.getBaseUri());
-            str = RequestUtils.mapToQueryString(command.getParams());
-            size = String.valueOf(str.length());
-            log.info("ArsenalpayAPI-SDK HttpUrlConnectionImpl URL:" + url.toString());
-            log.info("ArsenalpayAPI-SDK HttpUrlConnectionImpl Post Data: " + str);
-        } catch (Exception e) {
-            log.log(Level.SEVERE, "ArsenalpayAPI-SDK HttpUrlConnectionImpl:doPost", e);
-        }
-
-        try {
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Length", size);
-            if (configuration != null) {
-                connection.setReadTimeout(configuration.getReadTimeout());
-                connection.setConnectTimeout(configuration.getConnectionTimeout());
-            }
-
-            output = connection.getOutputStream();
-            printWriter = new PrintWriter(output);
-            printWriter.write(str);
-            printWriter.flush();
-        } catch (Exception e) {
-            log.log(Level.SEVERE, "ArsenalpayAPI-SDK HttpUrlConnectionImpl:doPost", e);
-        } finally {
-            if (printWriter != null) {
-                try {
-                    printWriter.close();
-                } catch (Exception e) {
-                    log.log(Level.SEVERE, "ArsenalpayAPI-SDK HttpUrlConnectionImpl:doPost", e);
-                }
-            }
-        }
-
+        HttpURLConnection connection = getConnection(command);
         try {
             return getResponse(connection);
-        } catch (Exception e) {
-            log.log(Level.SEVERE, "ArsenalpayAPI-SDK HttpUrlConnectionImpl:doPost", e);
         } finally {
-            connection.disconnect();
+            if (connection != null)
+                connection.disconnect();
         }
-
-        return ApiResponseImpl.createEmpty();
     }
 
 
@@ -155,6 +100,54 @@ public class HttpUrlConnectionImpl implements ApiClient {
         log.log(Level.INFO, "ArsenalpayAPI-SDK Response data is empty");
         return ApiResponseImpl.createEmpty();
 
+    }
+
+    private HttpURLConnection getConnection(ApiCommand command) throws IOException {
+        switch (command.getHttpMethod()) {
+            case GET: {
+                URL url = new URL(command.getFullUri());
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                if (configuration != null) {
+                    connection.setReadTimeout(configuration.getReadTimeout());
+                    connection.setConnectTimeout(configuration.getConnectionTimeout());
+                }
+                return connection;
+            }
+            case POST: {
+                PrintWriter printWriter = null;
+
+                URL url = new URL(command.getBaseUri());
+                String str = RequestUtils.mapToQueryString(command.getParams());
+                String size = String.valueOf(str.length());
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Length", size);
+                if (configuration != null) {
+                    connection.setReadTimeout(configuration.getReadTimeout());
+                    connection.setConnectTimeout(configuration.getConnectionTimeout());
+                }
+                try {
+                    printWriter = new PrintWriter(connection.getOutputStream());
+                    printWriter.write(str);
+                    printWriter.flush();
+                } finally {
+                    if (printWriter != null) {
+                        try {
+                            printWriter.close();
+                        } catch (Exception e) {
+                            log.log(Level.SEVERE, "ArsenalpayAPI-SDK HttpUrlConnectionImpl:doPost", e);
+                        }
+                    }
+                }
+
+                return connection;
+            }
+            default:
+                return null;
+        }
     }
 
 }
